6ad56a37db8b530ca962cc5cc1c9b08e2357338c
RoR9 <valeriu.verebceanu@gmail.com>
2022-08-21 13:26:48 +0300

    Header and Swiper

---
+++ script.js
@@ -0,0 +1,23 @@
+//Swiper
+const swiperOptions={
+    simulateTouch:true,
+
+    //touchRatio:1,
+  
+   // touchAngle:45,
+    //spaceBetween:30,
+  
+    //grabCursor:true,
+   // autoHeight:true,
+   slidesPerView:3,
+   spaceBetween:0,
+   loop:true,
+   speed:3500,
+   autoplay: {
+          delay: 0,
+          //disableOnInteraction:false,
+        
+      
+        },
+}
+new Swiper(".swiper",swiperOptions)
